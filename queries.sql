b0fbd44432e45615f0d73030ef5349f7cb3afd9c
Tommy Sutherland <tsutherland3530@gmail.com>
2024-06-26 05:18:48 -0400

    Add SQL queries for customer name and supplier address

---
+++ queries.sql
@@ -0,0 +1,13 @@
+﻿-- queries.sql
+
+-- Query to select the Customer Name for the OrderID 10310
+SELECT Customers.CustomerName
+FROM Orders
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.OrderID = 10310;
+
+-- Query to select the address for the supplier of ProductID 40
+SELECT Suppliers.Address
+FROM Products
+JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+WHERE Products.ProductID = 40;
